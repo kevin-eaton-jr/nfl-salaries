24408831fc16bd7450a09cf0adbd851560b20bd8
kevin-eaton-jr <159560429+kevin-eaton-jr@users.noreply.github.com>
2024-02-12 07:57:38 -0600

    Update script.py

--- script.py
+++ script.py
@@ -194,9 +194,9 @@ def_pos = ['CB', 'DE', 'DT', 'FS', 'ILB', 'LB', 'OLB', 'S', 'SS']
 
 st_pos = ['K', 'LS', 'P']
 
-average_starter_sal['Unit'] = 'OFF'
+average_starter_sal['Unit'] = 'DEF'
 
-#average_starter_sal['Unit'][average_starter_sal['Pos'] in off_pos] = 'OFF'
+average_starter_sal['Unit'][average_starter_sal['Pos'] in off_pos] = 'OFF'
 
 #average_starter_sal['Unit'] = 'ST' if average_starter_sal['Pos'] in st_pos else pass
 
